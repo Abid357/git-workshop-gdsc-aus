a80efe46a91170c57c2e29160531746c2c3d1ed9
Abid357 <abid357@hotmail.com>
2022-06-18 01:33:21 +0400

    added 1 phrase, total 4

--- session3.py
+++ session3.py
@@ -3,5 +3,6 @@ movie_phrases = list()
 movie_phrases.append("it's morbin' time")
 movie_phrases.append("harry, you're a wizard!")
 movie_phrases.append("hello there!")
+movie_phrases.append("i'm something of a scientist myself")
 
 print(movie_phrases)
