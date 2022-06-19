43ea9e1d10860e5d063f305c48f656fdabc5c9c9
Mohammed Elnawawy <melnawawy@aus.edu>
2022-06-19 15:26:52 +0400

    added 2 phrases

--- session3.py
+++ session3.py
@@ -7,6 +7,9 @@ movie_phrases.append("harry, you're a wizard!")
 movie_phrases.append("hello there!")
 movie_phrases.append("i'm something of a scientist myself")
 movie_phrases.append("why not you stupid bastard?")
+movie_phrases.append("he who must not be named")
+movie_phrases.append("The closer you think you are, the less you'll 
+actually see")
 
 # display the list of phrases
 print(movie_phrases)
