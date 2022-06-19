edb60820591be5ac4fe30e054692d7ff73ae867a
Abid357 <abid357@hotmail.com>
2022-06-19 14:42:16 +0400

    added 1 phrase, total 2

--- session3.py
+++ session3.py
@@ -3,6 +3,7 @@ movie_phrases = list()
 
 # append each phrase to the list
 movie_phrases.append("it's morbin' time")
+movie_phrases.append("why not you stupid bastard?")
 
 # display the list of phrases
 print(movie_phrases)
