2aa45eb5815cf6d3138db30084d0e2f84b926a62
Mohammed Elnawawy <melnawawy@aus.edu>
2022-06-19 15:28:06 +0400

    removed new line

--- session3.py
+++ session3.py
@@ -8,8 +8,7 @@ movie_phrases.append("hello there!")
 movie_phrases.append("i'm something of a scientist myself")
 movie_phrases.append("why not you stupid bastard?")
 movie_phrases.append("he who must not be named")
-movie_phrases.append("The closer you think you are, the less you'll 
-actually see")
+movie_phrases.append("The closer you think you are, the less you'll actually see")
 
 # display the list of phrases
 print(movie_phrases)
