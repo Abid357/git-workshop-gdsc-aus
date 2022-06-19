7c17fd810b7e5c9caa7e8573902fb3ba90b387f2
Omar <omarsafwat11@gmail.com>
2022-06-19 15:17:52 +0400

    added 2 quotes

--- session3.py
+++ session3.py
@@ -7,6 +7,8 @@ movie_phrases.append("harry, you're a wizard!")
 movie_phrases.append("hello there!")
 movie_phrases.append("i'm something of a scientist myself")
 movie_phrases.append("why not you stupid bastard?")
+movie_phrases.append("I'm gonna make him an offer he can't refuse.")
+movie_phrases.append("sheeeeeesh")
 
 # display the list of phrases
 print(movie_phrases)
