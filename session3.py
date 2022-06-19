fec51b1a6fc733d2b3e9b369c9f9e63e516328c4
Baibaros21 <alaatarek20@gmail.com>
2022-06-19 15:22:29 +0400

    Alaa phrases

--- session3.py
+++ session3.py
@@ -7,6 +7,9 @@ movie_phrases.append("why not you stupid bastard?")
 movie_phrases.append("harry, you're a wizard!")
 movie_phrases.append("hello there!")
 movie_phrases.append("i'm something of a scientist myself")
+movie_phrases.append("Why so serious")
+movie_phrases.append("you know nothing John Snow")
+
 
 # display the list of phrases
 print(movie_phrases)
