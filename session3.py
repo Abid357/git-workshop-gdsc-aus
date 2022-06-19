485115ce10253f0d5131942232ea898891b85eb8
Priya Bachan <bachanpriya20@gmail.com>
2022-06-19 17:05:47 +0530

    did

--- session3.py
+++ session3.py
@@ -7,6 +7,7 @@ movie_phrases.append("harry, you're a wizard!")
 movie_phrases.append("hello there!")
 movie_phrases.append("i'm something of a scientist myself")
 movie_phrases.append("why not you stupid bastard?")
-
+movie_phrases.append("It's not leviosAAA, it's leviOsa!")
+movie_phrases.append("I'll have a cappuchino.")
 # display the list of phrases
 print(movie_phrases)
