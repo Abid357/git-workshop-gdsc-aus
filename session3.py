91ffca7bc6e1cd79943e9245a909405775c9bbe2
Vibha Bhavikatti <vibha.bhavikatti@gmail.com>
2022-06-19 15:27:23 +0400

    added phrase by Vibha Bhavikatti

--- session3.py
+++ session3.py
@@ -7,6 +7,8 @@ movie_phrases.append("why not you stupid bastard?")
 movie_phrases.append("harry, you're a wizard!")
 movie_phrases.append("hello there!")
 movie_phrases.append("i'm something of a scientist myself")
+movie_phrases.append("It's alive")
+movie_phrases.append("hello world")
 
 # display the list of phrases
 print(movie_phrases)
